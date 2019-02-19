68157830ebbb94a441d103f6658e41b32f6ae204
Nikola Spalevic <nikola.s@vivifyideas.com>
2019-02-19 19:43:16 +0100

    Commit 21

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,12 +13,18 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(App\User::class, function (Faker\Generator $faker) {
-    static $password;
-
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'password' => bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Post::class, function (Faker\Generator $faker) {
+    return [
+        'title' => $faker->realText(150),
+        'body' => implode(' ', $faker->sentences(15)),
+        'published' => true
     ];
 });
